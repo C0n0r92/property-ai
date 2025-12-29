650cfcf25ff6830b73e1b8a2dac97bdf5d34543d
Conor McLoughlin <conor.mcloughlin@workday.com>
2025-12-29 15:15:38 +0000

    blog

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -155,7 +155,7 @@ export default function RootLayout({
           <BottomNav />
         </Providers>
         {/* Buy Me a Coffee widget */}
-        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="conor.mcloughlin" data-description="Support me on Buy me a coffee!" data-message="" data-color="#40DCA5" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="conor.mcloughlin" data-description="Support me on Buy me a coffee!" data-message="" data-color="#40DCA5" data-position="BottomLeft" data-x_margin="18" data-y_margin="80"></script>
       </body>
     </html>
   );
